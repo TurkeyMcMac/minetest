c430f092645b1c58c3dd64e65a507975db84026a
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2022-11-25 07:02:42 -0500

    Try removing camera_btn and later

--- src/gui/touchscreengui.cpp
+++ src/gui/touchscreengui.cpp
@@ -542,9 +542,11 @@ void TouchScreenGUI::init(ISimpleTextureSource *tsrc)
 	m_settingsbar.addButton(noclip_id,  L"noclip",    "noclip_btn.png");
 	m_settingsbar.addButton(fast_id,    L"fast",      "fast_btn.png");
 	m_settingsbar.addButton(debug_id,   L"debug",     "debug_btn.png");
+#if 0
 	m_settingsbar.addButton(camera_id,  L"camera",    "camera_btn.png");
 	m_settingsbar.addButton(range_id,   L"rangeview", "rangeview_btn.png");
 	m_settingsbar.addButton(minimap_id, L"minimap",   "minimap_btn.png");
+#endif
 
 	// Chat is shown by default, so chat_hide_btn.png is shown first.
 	m_settingsbar.addToggleButton(toggle_chat_id, L"togglechat",
