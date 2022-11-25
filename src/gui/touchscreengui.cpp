122e0c3a255af858fdfcfd7c400eb512e69715ca
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2022-11-25 07:22:04 -0500

    Try re-adding camera_btn

--- src/gui/touchscreengui.cpp
+++ src/gui/touchscreengui.cpp
@@ -542,8 +542,8 @@ void TouchScreenGUI::init(ISimpleTextureSource *tsrc)
 	m_settingsbar.addButton(noclip_id,  L"noclip",    "noclip_btn.png");
 	m_settingsbar.addButton(fast_id,    L"fast",      "fast_btn.png");
 	m_settingsbar.addButton(debug_id,   L"debug",     "debug_btn.png");
-#if 0
 	m_settingsbar.addButton(camera_id,  L"camera",    "camera_btn.png");
+#if 0
 	m_settingsbar.addButton(range_id,   L"rangeview", "rangeview_btn.png");
 	m_settingsbar.addButton(minimap_id, L"minimap",   "minimap_btn.png");
 #endif
